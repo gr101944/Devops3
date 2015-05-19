8b2c3056219aa7f73ac72ab5f836e3459f1b3155
unknown <a600974@MC0WX5SC.ww930.my-it-solutions.net>
2015-05-19 12:25:00 +0530

    test

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /usr/share/tomcat7/webapps/target/BankSample-1.0.war /usr/share/tomcat7/webapps
+cp /usr/share/tomcat7/webapps/target/BankSample-1.0.war /usr/share/tomcat7/webapps/BankSample.war
 service httpd restart > /var/log/restartapache.out 2>&1
